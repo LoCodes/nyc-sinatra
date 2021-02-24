6e35852dcfd6d9f2b7968d7a233e8a21218c145a
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-23 17:24:56 -0800

    commit tree 401ee2f9f246d7fe8ab6b8c369de4961f57b1c16

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,66 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  #allows you to view "first page"
+  get '/figures' do 
+    @figures = Figure.all   #needed for interpolation in view  
+    erb :'figures/index'    #iterpolated firgures because we rendered it in the view 
+  end 
+
+
+  # allows you to view form to create a new figure
+  get '/figures/new' do 
+    erb :'figures/new'     # render new form 
+  end 
+
+
+  # allows you to create a new figure with a title
+  # allows you to create a new figure with a landmark
+  # allows you to create a new figure with a new title
+  # allows you to create a new figure with a new landmark
+  # creates checkboxes for all the landmarks and titles created on the Figures new page
+  # allows you to list all figures
+
+
+  post '/figures' do
+    @figure = Figure.create(params['figure'])   
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  #allows you to see a single Figure
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  # allows you to view form to edit a single figure
+  get '/figures/:id/edit' do 
+    @figure =  Figure.find(params[:id])
+    erb :'/figures/edit'
+  end 
+
+  # allows you to edit a single figure
+  # shows you edited page 
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,45 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  # LandmarksController allows you to list all landmarks
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'landmarks/index'
+  end
+
+  # allows you to view form to create a new landmark
+  # allows you to create a new landmark
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  # allows you to create a new landmark
+  post '/landmarks' do
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    redirect '/landmarks'
+  end
+
+
+  # allows you to see a single landmark
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
+
+  # allows you to view the form to edit a single landmark (FAILED - 1)
+  # allows you to edit a single landmark
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  # shows landmark with edited value 
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
